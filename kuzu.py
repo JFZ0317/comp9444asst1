d31d966048802e621a21738e2da4007dbac39752
Jason Fang <zfan8110@uni.sydney.edu.au>
2024-06-22 19:22:30 +1000

    part 1 finished, need optimization

--- kuzu.py
+++ kuzu.py
@@ -12,26 +12,39 @@ class NetLin(nn.Module):
     # linear function followed by log_softmax
     def __init__(self):
         super(NetLin, self).__init__()
-        # INSERT CODE HERE
+        self.fc = nn.Linear(28 * 28, 10)
 
     def forward(self, x):
-        return 0 # CHANGE CODE HERE
+        x = x.view(-1, 28 * 28)
+        x = self.fc(x)
+        return F.log_softmax(x, dim=1)
 
 class NetFull(nn.Module):
     # two fully connected tanh layers followed by log softmax
-    def __init__(self):
+    def __init__(self, input_size=28*28, hidden_size=100, num_classes=10):
         super(NetFull, self).__init__()
-        # INSERT CODE HERE
+        self.fc1 = nn.Linear(input_size, hidden_size)
+        self.fc2 = nn.Linear(hidden_size, num_classes)
 
     def forward(self, x):
-        return 0 # CHANGE CODE HERE
+        x = x.view(-1, 28 * 28)
+        x = torch.tanh(self.fc1(x))
+        x = torch.tanh(self.fc2(x))
+        return F.log_softmax(x, dim=1)
 
 class NetConv(nn.Module):
     # two convolutional layers and one fully connected layer,
     # all using relu, followed by log_softmax
     def __init__(self):
         super(NetConv, self).__init__()
-        # INSERT CODE HERE
+        self.conv1 = nn.Conv2d(1, 32, kernel_size=3, stride=1, padding=1)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1)
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
+        self.fc1 = nn.Linear(64 * 7 * 7, 10)
 
     def forward(self, x):
-        return 0 # CHANGE CODE HERE
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = x.view(-1, 64 * 7 * 7)
+        x = self.fc1(x)
+        return F.log_softmax(x, dim=1)
